161ef163f4dad630a6d6a3e14f7c11f5987a0103
= <10166308+eurubkov@users.noreply.github.com>
2022-05-21 20:42:27 -0700

    Add logic for placing cards in the piles

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,22 @@ import { INVALID_MOVE } from 'boardgame.io/core';
 const DECK_SIZE = 98;
 const HAND_SIZE = 7;
 
+const FIRST_UP = "first_up";
+const SECOND_UP = "second_up";
+const FIRST_DOWN = "first_down";
+const SECOND_DOWN = "second_down";
+
+const UP_PILES = [FIRST_UP, SECOND_UP];
+const DOWN_PILES = [FIRST_DOWN, SECOND_DOWN];
+
+const PILES_MAP = {
+    [FIRST_UP]: 0,
+    [SECOND_UP]: 1,
+    [FIRST_DOWN]: 2,
+    [SECOND_DOWN]: 3
+}
+
+
 const DrawCard = (G, ctx) => {
     const card = G.deck.pop();
     G.hands[ctx.currentPlayer].push(card);
@@ -14,12 +30,33 @@ const Replenish = (G, ctx) => {
     }
 }
 
-const PlayCard = (G, ctx, card) => {
+const PlayCard = (G, ctx, card, pile) => {
     const cardIndex = G.hands[ctx.currentPlayer].indexOf(card);
-    if (cardIndex < 0) {
+    if (cardIndex < 0 || !CanPlayCard(G, ctx, card, pile)) {
         return INVALID_MOVE;
     }
     G.hands[ctx.currentPlayer].splice(cardIndex, 1);
+    G.piles[PILES_MAP[pile]] = card;
+}
+
+const CanPlayCard = (G, ctx, card, pile) => {
+    console.log(card)
+    console.log(pile)
+    if (UP_PILES.includes(pile)) {
+        const pileIndex = PILES_MAP[pile];
+        if (card > G.piles[pileIndex] || G.piles[pileIndex] - card === 10) {
+            return true;
+        }
+        return false;
+    } else if (DOWN_PILES.includes(pile)) {
+        const pileIndex = PILES_MAP[pile];
+        if (card < G.piles[pileIndex] || card - G.piles[pileIndex] === 10) {
+            return true;
+        }
+        return false;
+    } else {
+        return false;
+    }
 }
 
 
@@ -27,7 +64,8 @@ const PlayCard = (G, ctx, card) => {
 export const TheGame = {
     setup: (ctx) => ({
         deck: ctx.random.Shuffle(Array.from({ length: DECK_SIZE }, (v, i) => i + 2)),
-        hands: Array(2).fill([])
+        hands: Array(2).fill([]),
+        piles: [1, 1, 100, 100]
     }),
 
     phases: {

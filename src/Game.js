0d4ac09343ba52c14464b331c24db736d904399d
= <10166308+eurubkov@users.noreply.github.com>
2022-05-21 19:27:06 -0700

    Set the draw stage to automatically deal cards to players

--- src/Game.js
+++ src/Game.js
@@ -3,9 +3,12 @@ const DrawCard = (G, ctx) => {
     G.hands[ctx.currentPlayer].push(card);
 }
 
+const DECK_SIZE = 98;
+const HAND_SIZE = 7;
+
 export const TheGame = {
     setup: (ctx) => ({
-        deck: ctx.random.Shuffle(Array.from({ length: 98 }, (v, i) => i + 2)),
+        deck: ctx.random.Shuffle(Array.from({ length: DECK_SIZE }, (v, i) => i + 2)),
         hands: Array(2).fill([])
     }),
 
@@ -13,8 +16,15 @@ export const TheGame = {
         draw: {
             moves: { DrawCard },
             start: true,
-            endIf: G => (G.deck.length <= 98 - 14),
-            turn: { minMoves: 1, maxMoves: 1 }
+            endIf: (G, ctx) => (G.deck.length <= DECK_SIZE - ctx.numPlayers * HAND_SIZE),
+            onBegin: (G, ctx) => {
+                while (G.deck.length > DECK_SIZE - ctx.playOrder.length * HAND_SIZE) {
+                    for (let i = 0; i < ctx.numPlayers; i++) {
+                        const card = G.deck.pop();
+                        G.hands[ctx.playOrder[i]].push(card);
+                    }
+                }
+            }
         }
     }
 };

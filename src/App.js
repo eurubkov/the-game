fc1189d01d498dc04988b581649df1108a834073
= <10166308+eurubkov@users.noreply.github.com>
2022-05-29 23:24:06 -0700

    Use playerID instead of ctx.currentPlayer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { Client } from "boardgame.io/react";
 import TheGameBoard from "./Board";
 import { TheGame } from "./Game"
+import { Local } from 'boardgame.io/multiplayer'
 
-const App = Client({ game: TheGame, board: TheGameBoard });
+const TheGameClient = Client({ game: TheGame, board: TheGameBoard, multiplayer: Local() });
+const App = () => (
+    <div>
+        <TheGameClient playerID="0" />
+        <TheGameClient playerID="1" />
+    </div>
+);
 export default App;
--- src/Board.js
+++ src/Board.js
@@ -22,7 +22,7 @@ const INDEX_TO_PILE_MAP = {
 
 
 
-const TheGameBoard = ({ ctx, G, moves }) => {
+const TheGameBoard = ({ ctx, G, moves, playerID }) => {
     const onDragDrop = (e) => {
         const card = e.dataTransfer.getData("card");
         const pile = INDEX_TO_PILE_MAP[e.target.id];
@@ -55,8 +55,8 @@ const TheGameBoard = ({ ctx, G, moves }) => {
     }
 
     let hand = [];
-    for (let i = 0; i < G.hands[ctx.currentPlayer].length; i++) {
-        hand.push((<div onDragStart={onDragCard} draggable="true" key={i} style={pileStyle}>{G.hands[ctx.currentPlayer][i]}</div>))
+    for (let i = 0; i < G.hands[playerID].length; i++) {
+        hand.push((<div onDragStart={onDragCard} draggable="true" key={i} style={pileStyle}>{G.hands[playerID][i]}</div>))
     }
     return (<div>
         <h3>Piles</h3>

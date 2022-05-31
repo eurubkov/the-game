f463065f2873f340375dba2e60013dcd19cf75f0
= <10166308+eurubkov@users.noreply.github.com>
2022-05-30 21:53:52 -0700

    add origins

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { Server, Origins } = require("boardgame.io/server");
 const { TheGame } = require("./src/Game");
 
 const server = Server({
-    games: [TheGame]
+    games: [TheGame],
+    origins: [Origins.LOCALHOST_IN_DEVELOPMENT, "https://multiplayer--heartfelt-platypus-4661a1.netlify.app"]
 });
 const PORT = process.env.PORT || 8000;
 server.run(PORT);

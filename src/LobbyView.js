ac0263104818b31f9bfe9ecdc1fa913e436ac8c6
= <10166308+eurubkov@users.noreply.github.com>
2022-11-20 18:25:00 -0800

    Switch from heroku to onrender

--- src/LobbyView.js
+++ src/LobbyView.js
@@ -2,7 +2,7 @@ import { Lobby } from 'boardgame.io/react';
 import { TheGame } from './Game';
 import TheGameBoard from './Board';
 
-const server = `https://the-game-100.herokuapp.com`;
+const server = `https://the-game-100.onrender.com`;
 const LobbyView = () => (<Lobby
     gameServer={server}
     lobbyServer={server}

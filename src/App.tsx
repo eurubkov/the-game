0bd966b84dc6cba0eca38c6b3f71632db80baf61
Evgeny Urubkov <10166308+eurubkov@users.noreply.github.com>
2023-09-11 17:46:12 -0700

    Remove unused import to fix netlify build

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import LobbyView from "./LobbyView";
 import TheGame from './TheGame';
 import LocalBoard from "./LocalBoard";
 import { Client } from 'boardgame.io/react';
-import { Local } from 'boardgame.io/multiplayer'
 import * as React from "react";
 
 const AppClient = Client({
     game: TheGame,
     board: LocalBoard,
-    numPlayers: 5
+    numPlayers: 2
 })
 
 const DEBUG_MODE = process.env.REACT_APP_DEBUG_MODE === 'true';

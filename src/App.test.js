22fcd616f723c018004b5ec09db40f00b5523b69
Evgeny Urubkov <10166308+eurubkov@users.noreply.github.com>
2025-07-11 22:05:50 -0700

    Revert "Implement the bot and allow to choose between multi-player and single game with bots"
    This reverts commit f19b4c704ba20ee6d278cb4a6d0a091b5425ed2c.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders The Game title', () => {
+test('renders learn react link', () => {
   render(<App />);
-  const titleElement = screen.getByText(/The Game/i);
-  expect(titleElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
